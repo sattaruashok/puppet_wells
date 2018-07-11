759c6b196a7beef653334bf5bab92531040c2ceb
sattaruashok <40977840+sattaruashok@users.noreply.github.com>
2018-07-11 09:42:01 +0000

    Update init.pp

--- init.pp
+++ init.pp
@@ -8,3 +8,27 @@ class http (
   class { '::http::install': } ->
   class { '::http::service': }
 
+
+class http::install(
+  $package_name = $::http::package_name,
+  $ensure       = $::http::ensure,
+) {
+  package { 'http-package':
+    ensure => $ensure,
+    name   => $package_name,
+  }
+}
+
+class http::service(
+ $service_name   = $::http::service_name,
+ $service_ensure = $::http::service_ensure,
+ $service_enable = $::http::service_enable,
+) {
+  service { 'http-service':
+    ensure     => $service_ensure,
+    name       => $service_name,
+    enable     => $service_enable,
+    hasstatus  => true,
+    hasrestart => true,
+  }
+}
